813c8593646bc1d5c190dad1cca864110bac3365
NIRMAL GHOSH <145506351+techno-ghosh@users.noreply.github.com>
2023-12-05 19:40:39 -0500

    second commit

--- manage.py
+++ manage.py
@@ -1,6 +1,22 @@
-# manage.py adding comment content to check auto restarting of the server.
+
+# manage.py
+import sys
 from flask.cli import FlaskGroup
-from src import app
-cli = FlaskGroup(app)
+from src import create_app, db # new
+from src.api.models import User # new
+app = create_app() # new
+cli = FlaskGroup(create_app=create_app) # new
+@cli.command('recreate_db')
+def recreate_db():
+ db.drop_all()
+ db.create_all()
+ db.session.commit()
+
+@cli.command('seed_db')
+def seed_db():
+ db.session.add(User(username='john', email="john@algonquincollege.com"))
+ db.session.add(User(username='Dave', email="dave@algonquincollege.com"))
+ db.session.commit() 
+
 if __name__ == '__main__':
  cli()
--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,29 @@
 # src/__init__.py
-from flask import Flask, jsonify
-from flask_restx import Resource, Api
-# instantiate the app
-app = Flask(__name__)
-api = Api(app)
-# set config
-app.config.from_object('src.config.DevelopmentConfig') # new
-class Ping(Resource):
- def get(self):
-    return {
-    'status': 'success',
-    'message': 'pong!'
-    }
-api.add_resource(Ping, '/ping')
+import os
+from flask import Flask # new
+from flask_sqlalchemy import SQLAlchemy
+# instantiate the db
+db = SQLAlchemy()
+# new
+def create_app(script_info=None):
+ # instantiate the app
+ app = Flask(__name__)
+ # set config
+ app_settings = os.getenv('APP_SETTINGS')
+ app.config.from_object(app_settings)
+ # set up extensions
+ db.init_app(app)
+ # register blueprints
+ from src.api.ping import ping_blueprint
+ app.register_blueprint(ping_blueprint)
+ 
+ from src.api.users import users_blueprint
+ app.register_blueprint(users_blueprint)
+
+
+ # shell context for flask cli
+ @app.shell_context_processor
+ 
+ def ctx():
+    return {'app': app, 'db': db}
+ return app
